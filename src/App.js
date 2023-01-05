18e0f332e9e221589d2ca5a5bdc0a089d8d7af11
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-01-05 16:48:07 +0100

    App js edited

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import Home from "./routes/home/home.component";
 import {Route, Routes} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Shop from "./routes/shop/shop.component";
-<<<<<<< HEAD
 import Authentication from "./routes/sign-in/auth.component";
-=======
 import SignIn from "./routes/sign-in/sign-in.component";
->>>>>>> main
 
 
 function App() {
@@ -15,11 +12,8 @@ function App() {
         <Route path='/' element={<Navigation></Navigation>}>
             <Route index={true} element={<Home></Home>}></Route>
             <Route path='shop' element={<Shop></Shop>}></Route>
-<<<<<<< HEAD
             <Route path='auth' element={<Authentication></Authentication>}></Route>
-=======
             <Route path='sign-in' element={<SignIn></SignIn>}></Route>
->>>>>>> main
         </Route>
     </Routes>
 }
